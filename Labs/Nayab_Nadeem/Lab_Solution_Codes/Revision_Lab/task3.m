31f8d839c17bffa9c5877d5465f6899e2d06908e
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-07-07 16:00:38 +0400

    update

---
+++ Labs/Nayab_Nadeem/Lab_Solution_Codes/Revision_Lab/task3.m
@@ -0,0 +1,19 @@
+% REVISION LAB - TASK 3
+
+% Create a numerical array of the information provided
+% Multiply all the values of the array by 6 and then find 
+% the square root of each value
+
+% -------------------------------------------------------------------------
+
+% Define the numerical array with the information provided
+num_array = [2 3 6 7 9 8 2 1];
+
+% Perform arithmetic operations on the array
+updated_array = sqrt(num_array * 6);
+
+% Display the arrays (original and updated)
+disp('Original Array:');
+disp(num_array);
+disp('Updated Array:');
+disp(updated_array);
