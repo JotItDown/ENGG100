dee53fb2ac30a3fd985a4b58ad0504ab28690031
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-07-07 15:58:52 +0400

    update

---
+++ Labs/Nayab_Nadeem/Lab_Solution_Codes/Lab_1/Lab1_Task1.m
@@ -0,0 +1,17 @@
+% LAB 1 - TASK 1
+
+% A car is traveling 10.2 miles/hour. Convert this speed to
+% kilometers/hour.
+
+
+% -------------------------------------------------------------------------
+
+% Set the conversion rate
+km_to_mph = 1.6;
+
+% Convert 10.2 mph to kph
+kph = 10.2 * km_to_mph;
+
+% Display the final value
+disp(['10.2 miles/hour is ', num2str(kph),' kilometers/hour.']);
+
