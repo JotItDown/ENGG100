32118c669533f64de8ee5824538488f762774f55
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-07-07 15:59:43 +0400

    update

---
+++ Labs/Nayab_Nadeem/Lab_Solution_Codes/Lab_5/Lab5_Task2.m
@@ -0,0 +1,12 @@
+% LAB 5 - TASK 2
+
+% Create a for loop to display some text (refer to Lab Presentation)
+
+% -------------------------------------------------------------------------
+
+% Create a for loop that runs 10 times, with a stepper of 1
+for n = 1:1:10
+    
+    % In each iteration, display a line
+    fprintf("This is line %d \n", n);
+end
