c90b595df88992fa1c978c773530d032d527fe4e
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-07-07 15:59:25 +0400

    update

---
+++ Labs/Nayab_Nadeem/Lab_Solution_Codes/Lab_3/Lab3_Task5.m
@@ -0,0 +1,23 @@
+% LAB 3 - TASK 5
+
+% Create a timetable using cell arrays (use curly brackets to define cell
+% arrays)
+% Hint: new columns can be defined with commas & spaces whilst rows are
+% defined with semicolons.
+
+% -------------------------------------------------------------------------
+
+timetable = {
+
+'', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday';
+'8:30 - 9:30','','','','','';
+'9:30 - 10:30','','','','','';
+'10:30 - 11:30','','','','','';
+'11:30 - 12:30','','','','','';
+'12:30 - 13:30','','','','','';
+'13:30 - 14:30','','','','','';
+'14:30 - 15:30','','','','','';
+'15:30 - 16:30','','','','','';
+'16:30 - 17:30','','','','','';
+'17:30 - 18:30','','','','','';
+};
