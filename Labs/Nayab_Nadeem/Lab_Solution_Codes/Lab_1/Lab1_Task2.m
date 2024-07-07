23a84eaadf7041f405d0024868525be6da7f398f
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-07-07 15:58:55 +0400

    update

---
+++ Labs/Nayab_Nadeem/Lab_Solution_Codes/Lab_1/Lab1_Task2.m
@@ -0,0 +1,15 @@
+% LAB 1 - TASK 2
+
+% Multiply your student number with pi, find the square root, 
+% and assign it to a variable student_id
+
+% -------------------------------------------------------------------------
+
+% Create a variable and store your student number
+student_number = 1234567;
+
+% Calculate student_id using the equation given in the question
+student_id = sqrt(student_number * pi);
+
+% Display the final value
+disp(['student_id is ', num2str(student_id)]);
